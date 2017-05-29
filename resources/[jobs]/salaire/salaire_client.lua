eadaf04935b77d6a88cb2860864695003356bfb8
Xavier Chopin <xchopin54@gmail.com>
2017-05-29 14:37:57 +0200

    Fixed payday (#6)
    Salaire réactivé, car ils étaient désactivé au lancement.
    et ajout de l'auteur du script dans le code :) !
    
    Merci xchopin!

--- resources/[jobs]/salaire/salaire_client.lua
+++ resources/[jobs]/salaire/salaire_client.lua
@@ -1,6 +1,7 @@
+-- Author <https://github.com/PandaBasketteur>
 Citizen.CreateThread(function ()
 	while true do
-	Citizen.Wait(600000) -- Modifier cette valeur pour la fréquence des salaires (600000 = 10 minutes)
-		-- TriggerServerEvent('paycheck:salary')
+	    Citizen.Wait(600000) -- Modifier cette valeur pour la fréquence des salaires (600000 = 10 minutes)
+	    TriggerServerEvent('paycheck:salary')
 	end
 end)
--- resources/[jobs]/salaire/salaire_server.lua
+++ resources/[jobs]/salaire/salaire_server.lua
@@ -11,9 +11,9 @@ AddEventHandler('paycheck:salary', function()
   	-- Requête qui permet de recuperer le métier de l'utilisateur
   	local executed_query = MySQL:executeQuery("SELECT salary FROM users INNER JOIN jobs ON users.job = jobs.job_id WHERE identifier = '@username'",{['@username'] = user_id})
     local result = MySQL:getResults(executed_query, {'salary'})
-    local salary_job = result[1].salary
+    local salary_job = tonumber(result[1].salary)
     if salary_job > 0 then
-      user:addMoney((salary + salary_job))
+          user:addMoney((salary + salary_job))
    	  TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Salaire reçu :  + "..salary.."~g~$~s~~n~Salaire metier reçu : + "..salary_job.." ~g~$")
     end
  	end)
