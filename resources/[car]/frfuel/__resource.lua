91d1d1c02ec508e0486ee396f937ed34f39ffca0
Louis-David Lacerte <ldlacerte@hotmail.com>
2017-05-30 23:48:36 -0400

    En cours du merge de la job emergency. Pas complet.

--- resources/[car]/frfuel/__resource.lua
+++
@@ -1,3 +0,0 @@
-client_script "frfuel.net.dll"
-server_script "price.lua"
-file "config.ini"
--- resources/[citizenv]/foodhud/__resource.lua
+++ resources/[citizenv]/foodhud/__resource.lua
@@ -1,5 +1,6 @@
 client_script "foodhud.net.dll"
 client_script "frfuel.net.dll"
+client_script "price.lua"
 file "foodhud_config.ini"
 file "config.ini"
 
